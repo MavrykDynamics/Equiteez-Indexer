4813d60b923bbdacdfe92f6574d8967daaf109fa
0xVoronov <johnvoronov@gmail.com>
2026-06-02 06:56:56 +0300

    Fixed linter

--- equiteez/hooks/refresh_launchpad_stats.py
+++ equiteez/hooks/refresh_launchpad_stats.py
@@ -8,5 +8,7 @@ logger = logging.getLogger(__name__)
 
 async def refresh_launchpad_stats(ctx: HookContext) -> None:
     conn = Tortoise.get_connection("default")
-    await conn.execute_query("REFRESH MATERIALIZED VIEW CONCURRENTLY launchpad_launch_stats")
+    await conn.execute_query(
+        "REFRESH MATERIALIZED VIEW CONCURRENTLY launchpad_launch_stats"
+    )
     logger.debug("Refreshed launchpad_launch_stats")
